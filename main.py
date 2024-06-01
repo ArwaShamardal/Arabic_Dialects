705e14b3a72d1c24fac1c670b6bd8205e2bc9a09
ArwaShamardal <76781020+ArwaShamardal@users.noreply.github.com>
2024-06-01 22:46:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+
 
 from scripts.data_fetching import DBHandler
 from scripts.data_preprocessing import Preprocess
 
+
+# NOTE:: the commented out part should only run once
 # database_path = './data/dialects_database.db'
 # db_handler = DBHandler(database_path)
 
@@ -23,3 +27,6 @@ df_copy = df.copy()
 df_preprocessed = Preprocess(df_copy)
 df_preprocessed = df_preprocessed.preprocess()
 df_preprocessed.head()
+
+X_train, X_test, y_train, y_test = train_test_split(
+    df_preprocessed['text'], df_preprocessed['dialect'], test_size=0.2, random_state=42)
