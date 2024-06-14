d9054e5e49461e5b3d2cb64d4c530ec1b226258e
ArwaShamardal <76781020+ArwaShamardal@users.noreply.github.com>
2024-06-14 09:20:26 +0300

    Added dropdown
    Added dropdown  and connected the input to the app fil

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
 from flask import Flask, request, jsonify, render_template
 
+from scripts.data_fetching import DBHandler
+from scripts.data_preprocessing import Preprocess
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.linear_model import LogisticRegression
+from scripts.ml_model import MLModel
+# from scripts.dl_model import NLPModel
+import os
+
+
+# model_ml = MLModel.load_model('./models/logistic_regression_model.pkl')
+# model_dl = NLPModel.load_model('./models/dl_model.pkl')
+
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    model_names = []
+    for file in os.listdir('./models'):
+        if file.endswith('.pkl'):
+            name = file.split('.')[0]
+            model_names.append(name)
+
+    return render_template("home.html", models=model_names)
 
 
 if __name__ == '__main__':
